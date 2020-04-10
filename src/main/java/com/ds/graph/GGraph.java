87684a7bb9ec60d315c0c7fe3efc5dd3a0dcde13
Arnab Saha <arsaha28@gmail.com>
2020-04-10 11:22:20 +0100

    Add BFS ,see why vertexcount is not working properly

--- src/main/java/com/ds/graph/GGraph.java
+++ src/main/java/com/ds/graph/GGraph.java
@@ -1,5 +1,6 @@
 package com.ds.graph;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class GGraph {
@@ -9,14 +10,34 @@ public class GGraph {
 	
 	public GGraph(int size) {
 		vertexCount = size;
-		adjacencyList = new LinkedList[vertexCount];
-		for(int i= 0;i<vertexCount;++i) {
+		adjacencyList = new LinkedList[10];
+		for(int i= 0;i<10;++i) {
 			adjacencyList[i] = new LinkedList<Integer>();
 		}	
 	}
 	
 	public void addEdge(int vertex,int otherVertex) {
 		adjacencyList[vertex].add(otherVertex);
+	}
+	
+	public void bfs(int vertex) {
+		
+		boolean isVisited[] = new boolean[10];
+		LinkedList<Integer> queue = new LinkedList<Integer>();
+		isVisited[vertex] = true;
+		queue.add(vertex);
+		while(queue.size()!=0) {
+			vertex = queue.poll();
+			System.out.println(vertex);
+			Iterator<Integer> itr = adjacencyList[vertex].listIterator();
+			while(itr.hasNext()) {
+				int adjacentVertex = itr.next();
+				if(!isVisited[adjacentVertex]) {
+					isVisited[adjacentVertex] = true;
+					queue.add(adjacentVertex);
+				}
+			}
+		}
 	}
 	
 }
